7ee82058ffeaff4b6b7a045b08cadfc9e2107ee1
sokeking0007 <103271639+sokeking0007@users.noreply.github.com>
2024-10-07 14:34:30 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,11 +7,19 @@ const res = await fetch(url)
 const dados = await res.json()
 console.log(dados);
 
+const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+
+const horas = parseInt(dados.tempo_medio)
+const minutos = Math.round ((dados.tempo_medio - horas)* 100).toFixed(2)
+
 const paragrafo = document.createElement("p")
 paragrafo.classList.add ("graficos-container__texto")
 
-paragrafo.innerHTML = `vocẽ sabia que o mundo tem 6 continentes ${dados.total_pessoas_conectadas}`
-
+paragrafo.innerHTML = `vocẽ sabia que o mundo tem 
+<span> ${pessoasNoMundo} </span> de pessoas e aproxima
+<span> ${pessoasConectadas} </span> estão conectadas
+e passam em media <span> ${horas} </span> : <span> ${minutos} </span> horas conectadas`
 
 console.log(paragrafo)
 
